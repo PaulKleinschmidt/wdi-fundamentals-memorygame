31deeb24b4d6145e678a9478780068567555604d
Paul Kleinschmidt <paul@Pauls-MacBook-Pro.local>
2017-10-14 19:46:44 -0400

    draw two cards and print result of comparison

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		return "You found a match!";
 	} else {
-		console.log("Sorry, try again.");
+		return "Sorry, try again.";
 	}
 }
 
@@ -20,14 +20,21 @@ var flipCard = function(cardId) {
 
 
 	cardsInPlay.push(cards[cardId]);
+
 	console.log("User flipped " + cards[cardId]);
-	console.log(checkForMatch())
+	
+	if (cardsInPlay.length == 1) {
+		return "Draw another card";
+	}
+	if (cardsInPlay.length == 2) {
+	  return checkForMatch();
+    } 
 
 
 }
 
 console.log(flipCard(0));
-console.log(flipCard(1));
+console.log(flipCard(3));
 
 
 
