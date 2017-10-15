b7ba6d146c6f5269e07e19506211d9dee1c95624
Paul Kleinschmidt <paul@Pauls-MacBook-Pro.local>
2017-10-14 23:16:25 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 
 
 
-var cards = ['queen','queen','king','king'];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+
+}
+];
 
 var cardsInPlay = [];
 
@@ -9,9 +32,9 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		return "You found a match!";
+		alert("You found a match!");
 	} else {
-		return "Sorry, try again.";
+		alert("Sorry, try again");
 	}
 }
 
@@ -19,9 +42,9 @@ var flipCard = function(cardId) {
 
 
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 	
 	if (cardsInPlay.length == 1) {
 		return "Draw another card";
@@ -31,10 +54,17 @@ var flipCard = function(cardId) {
     } 
 
 
+
 }
 
-console.log(flipCard(0));
-console.log(flipCard(3));
+console.log(flipCard(1));
+console.log((cards[1].suit))
+console.log((cards[1].cardImage))
+console.log(flipCard(1));
+console.log((cards[1].suit))
+console.log((cards[1].cardImage))
+
+
 
 
 
